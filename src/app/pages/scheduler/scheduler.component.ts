80609ddecf809113bb60d75c2f5895ad98ab9f14
Asma Ben Boubaker <36500896+asmabenboubaker@users.noreply.github.com>
2023-07-27 19:41:04 +0200

    Juge Update fixed

--- src/app/pages/scheduler/scheduler.component.ts
+++ src/app/pages/scheduler/scheduler.component.ts
@@ -10,7 +10,9 @@ import { DropDownList } from '@syncfusion/ej2-dropdowns';
 @Component({
   selector: 'app-scheduler',
   providers: [DayService, WeekService, WorkWeekService, MonthService],
-  template: `<ejs-schedule width='100%' height='550px' [readonly]="readonly" [selectedDate]="selectedDate" [eventSettings]="eventSettings" (popupOpen)='onPopupOpen($event)'>
+  template: `<ejs-schedule width='100%' height='550px' [readonly]="readonly" [selectedDate]="selectedDate" [eventSettings]="eventSettings" (popupOpen)='onPopupOpen($event)'
+  (dragStart) = "onDragStart($event)" [views]='scheduleViews'
+  >
     </ejs-schedule>`
 })
 export class SchedulerComponent implements OnInit{
@@ -29,22 +31,57 @@ export class SchedulerComponent implements OnInit{
     adaptor: new UrlAdaptor,
     crossDomain: true
   });
+  
   public eventSettings: EventSettingsModel = {
-    includeFiltersInQuery: true, dataSource: this.dataManager, fields: {
+     dataSource: this.dataManager, fields: {
+      juge :{name:'juge'},
       id: 'Id',
       subject: { name: 'subject' },
       location: { name: 'location' },
       description: { name: 'description' },
       startTime: { name: 'startTime' },
-      endTime: { name: 'endTime' }
-      // recurrenceRule: { name: 'ShipRegion' }
+      endTime: { name: 'endTime' },
+      recurrenceRule: { name: 'ShipRegion' },
+      
     }
   };
   // public eventSettings: EventSettingsModel = { dataSource: this.dataManager };
   onDragStart(args: DragEventArgs): void {
-    args.scroll = { enable: true, scrollBy: 5, timeDelay: 200 };
+    args.excludeSelectors = 'e-header-cells,e-header-day,e-header-date,e-all-day-cells';
 }
 // add field juge 
+// onPopupOpen(args: PopupOpenEventArgs): void {
+//   if (args.type === 'Editor') {
+//       // Create required custom elements in initial time
+//       if (!args.element.querySelector('.custom-field-row')) {
+//           let row: HTMLElement = createElement('div', { className: 'custom-field-row' });
+//           let formElement: HTMLElement = args.element.querySelector('.e-schedule-form') as HTMLElement;
+//           formElement.firstChild?.insertBefore(row, args.element.querySelector('.e-title-location-row'));
+//           let container: HTMLElement = createElement('div', { className: 'e-juge-container' });
+//           let inputEle: HTMLInputElement = createElement('input', {
+//               className: 'e-float-input e-control-wrapper e-input-group', attrs: { name: 'juge'}
+//           }) as HTMLInputElement;
+//           // Add the label
+//       let labelEle: HTMLElement = createElement('label', {
+//         innerHTML: 'Juge:',
+//         className: 'e-float-text e-label-top',
+//         id:'label_subject',
+//         attrs: { for: 'description' }
+//       });
+//        container.appendChild(labelEle);
+
+//           container.appendChild(inputEle);
+//           row.appendChild(container);
+    
+//           inputEle.setAttribute('name', 'juge');
+          
+//           // let eventObj: any = args.data;
+//           // // Set the value of the input field with the existing data
+//           // inputEle.value = eventObj.juge || '';
+//       }
+//   }
+// }
+ 
 onPopupOpen(args: PopupOpenEventArgs): void {
   if (args.type === 'Editor') {
       // Create required custom elements in initial time
@@ -52,28 +89,17 @@ onPopupOpen(args: PopupOpenEventArgs): void {
           let row: HTMLElement = createElement('div', { className: 'custom-field-row' });
           let formElement: HTMLElement = args.element.querySelector('.e-schedule-form') as HTMLElement;
           formElement.firstChild?.insertBefore(row, args.element.querySelector('.e-title-location-row'));
-          let container: HTMLElement = createElement('div', { className: 'e-description-container' });
+          let container: HTMLElement = createElement('div', { className: 'custom-field-container' });
           let inputEle: HTMLInputElement = createElement('input', {
-              className: 'e-float-input e-control-wrapper e-input-group', attrs: { name: 'juge',type: 'text','aria-labelledby': 'label_subject'}
+              className: 'e-field', attrs: { name: 'juge' }
           }) as HTMLInputElement;
-          // Add the label
-      let labelEle: HTMLElement = createElement('label', {
-        innerHTML: 'Juge:',
-        className: 'e-float-text e-label-top',
-        id:'label_subject',
-        attrs: { for: 'description' }
-      });
-      container.appendChild(labelEle);
-
           container.appendChild(inputEle);
           row.appendChild(container);
-    
+         
           inputEle.setAttribute('name', 'juge');
       }
   }
 }
- 
-
   
 
   initializeCalendar(): void {
