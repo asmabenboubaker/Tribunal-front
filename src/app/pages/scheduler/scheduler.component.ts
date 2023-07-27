f134f087424a101cf278ea2e6dde3a36d0e755ce
Asma Ben Boubaker <36500896+asmabenboubaker@users.noreply.github.com>
2023-07-27 22:06:14 +0200

    field validation

--- src/app/pages/scheduler/scheduler.component.ts
+++ src/app/pages/scheduler/scheduler.component.ts
@@ -34,56 +34,26 @@ export class SchedulerComponent implements OnInit{
   });
   
   public eventSettings: EventSettingsModel = {
-     dataSource: this.dataManager, fields: {
+    includeFiltersInQuery: true,dataSource: this.dataManager, fields: {
       juge :{name:'juge'},
       id: 'Id',
-      subject: { name: 'subject' },
-      location: { name: 'location' },
-      description: { name: 'description' },
-      startTime: { name: 'startTime' },
-      endTime: { name: 'endTime' },
+      subject: { name: 'subject' , title:'sujet',validation: { required: true } },
+      location: { name: 'location' , title:'tribunal',validation: { required: true } },
+      description: { name: 'description',validation: { required: true }  },
+      startTime: { name: 'startTime'  },
+      endTime: { name: 'endTime'},
       recurrenceRule: { name: 'ShipRegion' },
+      demandeur: { name: 'demandeur' },
+      defendeur: { name: 'defendeur' },
       
     }
   };
   // public eventSettings: EventSettingsModel = { dataSource: this.dataManager };
   onDragStart(args: DragEventArgs): void {
     args.excludeSelectors = 'e-header-cells,e-header-day,e-header-date,e-all-day-cells';
-}
-// add field juge 
-// onPopupOpen(args: PopupOpenEventArgs): void {
-//   if (args.type === 'Editor') {
-//       // Create required custom elements in initial time
-//       if (!args.element.querySelector('.custom-field-row')) {
-//           let row: HTMLElement = createElement('div', { className: 'custom-field-row' });
-//           let formElement: HTMLElement = args.element.querySelector('.e-schedule-form') as HTMLElement;
-//           formElement.firstChild?.insertBefore(row, args.element.querySelector('.e-title-location-row'));
-//           let container: HTMLElement = createElement('div', { className: 'e-juge-container' });
-//           let inputEle: HTMLInputElement = createElement('input', {
-//               className: 'e-float-input e-control-wrapper e-input-group', attrs: { name: 'juge'}
-//           }) as HTMLInputElement;
-//           // Add the label
-//       let labelEle: HTMLElement = createElement('label', {
-//         innerHTML: 'Juge:',
-//         className: 'e-float-text e-label-top',
-//         id:'label_subject',
-//         attrs: { for: 'description' }
-//       });
-//        container.appendChild(labelEle);
-
-//           container.appendChild(inputEle);
-//           row.appendChild(container);
-    
-//           inputEle.setAttribute('name', 'juge');
-          
-//           // let eventObj: any = args.data;
-//           // // Set the value of the input field with the existing data
-//           // inputEle.value = eventObj.juge || '';
-//       }
-//   }
-// }
+} 
  
-//underlined Juge field
+//underlined Juge et requérant field
 onPopupOpen(args: PopupOpenEventArgs): void {
   if (args.type === 'Editor') {
       // Create required custom elements in initial time
@@ -94,7 +64,7 @@ onPopupOpen(args: PopupOpenEventArgs): void {
           let container: HTMLElement = createElement('div', { className: 'custom-field-container' });
           //add a div with class " e-float-input e-control-wrapper e-input-group " inside container and ari
           let container2: HTMLInputElement = createElement('input', {
-              className: 'e-float-input e-control-wrapper e-input-group e-field', attrs: {title:'Juge', name: 'juge', 'aria-labelledby':"label_juge"}
+              className: 'e-float-input e-control-wrapper e-input-group e-field', attrs: {title:'Juge', name: 'juge', 'aria-labelledby':"label_juge",  placeholder: 'Juge' }
           }) as HTMLInputElement;
           
           
@@ -123,6 +93,68 @@ onPopupOpen(args: PopupOpenEventArgs): void {
           row.appendChild(container);
          
           inputEle.setAttribute('name', 'juge');
+          ////////////////////////////////////////////////////////////////////////////////////
+          let row1: HTMLElement = createElement('div', { className: 'custom-field-row' });
+          let formElement1: HTMLElement = args.element.querySelector('.e-schedule-form') as HTMLElement;
+          formElement1.firstChild?.insertBefore(row1, args.element.querySelector('.e-title-location-row'));
+          let container3: HTMLElement = createElement('div', { className: 'custom-field-container' });
+          //add a div with class " e-float-input e-control-wrapper e-input-group " inside container and ari
+          let container33: HTMLInputElement = createElement('input', {
+              className: 'e-float-input e-control-wrapper e-input-group e-field', attrs: {title:'demandeur', name: 'demandeur', 'aria-labelledby':"label_demandeur",  placeholder: 'demandeur' }
+          }) as HTMLInputElement;
+          
+          
+          let spanLine1: HTMLElement = createElement('span', { className: 'e-float-line' });
+          // add label with class e-float-text e-label-top 
+          let labelEle1: HTMLElement = createElement('label', {
+            innerHTML: 'demandeur:',
+            className: 'e-float-text e-label-top',
+            id:'label_demandeur',
+            attrs: { for: 'demandeur' }
+          });
+          
+          let inputEle1: HTMLInputElement = createElement('input', {
+              className: '', attrs: { name: 'demandeur' }
+          }) as HTMLInputElement;
+          
+          container.appendChild(container33);
+          container2.appendChild(inputEle1);
+          container2.appendChild(spanLine1);
+          container2.appendChild(labelEle1);
+          row.appendChild(container3);
+         
+          inputEle.setAttribute('name', 'demandeur');
+          //////////////////////////: //////////////////////////////////////////////////////
+          let row2: HTMLElement = createElement('div', { className: 'custom-field-row' });
+          let formElement2: HTMLElement = args.element.querySelector('.e-schedule-form') as HTMLElement;
+          formElement2.firstChild?.insertBefore(row2, args.element.querySelector('.e-title-location-row'));
+          let container4: HTMLElement = createElement('div', { className: 'custom-field-container' });
+          //add a div with class " e-float-input e-control-wrapper e-input-group " inside container and ari
+          let container44: HTMLInputElement = createElement('input', {
+              className: 'e-float-input e-control-wrapper e-input-group e-field', attrs: {title:'demandeur', name: 'defendeur', 'aria-labelledby':"label_defendeur",  placeholder: 'defendeur' }
+          }) as HTMLInputElement;
+          
+          
+          let spanLine2: HTMLElement = createElement('span', { className: 'e-float-line' });
+          // add label with class e-float-text e-label-top 
+          let labelEle2: HTMLElement = createElement('label', {
+            innerHTML: 'defendeur',
+            className: 'e-float-text e-label-top',
+            id:'label_defendeur',
+            attrs: { for: 'defendeur' }
+          });
+          
+          let inputEle2: HTMLInputElement = createElement('input', {
+              className: '', attrs: { name: 'defendeur' }
+          }) as HTMLInputElement;
+          
+          container.appendChild(container44);
+          container2.appendChild(inputEle2);
+          container2.appendChild(spanLine2);
+          container2.appendChild(labelEle2);
+          row.appendChild(container4);
+         
+          inputEle.setAttribute('name', 'defendeur');
       }
   }
 }
